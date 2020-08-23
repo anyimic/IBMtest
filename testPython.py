8154851129c0bfc42f0e5700c25fba51d47e57ad
anyimic <49086513+anyimic@users.noreply.github.com>
2020-08-22 18:50:13 -0700

    Update testPython.py

--- testPython.py
+++ testPython.py
@@ -1,2 +1,3 @@
 # Display to standard output
-print("For all the world is a stage")
+print("All the world's a stage, and all the men and women merely players. They have their exits and their entrances; And one man in his time plays many parts.")
+print("As you Like It: Act 2, Scene 7")
