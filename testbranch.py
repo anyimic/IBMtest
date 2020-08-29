6d4f7b617dcbd1d278cf8fd4ae680824ab5a6a4c
anyimic <49086513+anyimic@users.noreply.github.com>
2020-08-28 21:11:29 -0700

    Delete testbranch.py

--- testbranch.py
+++
@@ -1,4 +0,0 @@
-## Adding a new file to child branch
-print("Inside branch: firstbranch")
-print("Some are born great, some achieve greatness, and some have greatness thrust upon them.")
-print("Twelth Night: Act 2, Scene 5")
